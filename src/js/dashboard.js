7b84c85557568e84933162ee08fa07185ac5f3f7
jaceyy009 <120229247+jaceyy009@users.noreply.github.com>
2025-11-17 17:35:05 -0500

    Update and rename placeholder.txt to dashboard.js

---
+++ src/js/dashboard.js
@@ -0,0 +1,9 @@
+// Placeholder stats
+const totalRoutes = 2;
+const totalStops = 20;
+const totalAudioFiles = 15;
+
+// Set values in dashboard
+document.getElementById('total-routes').textContent = totalRoutes;
+document.getElementById('total-stops').textContent = totalStops;
+document.getElementById('total-audio').textContent = totalAudioFiles;
